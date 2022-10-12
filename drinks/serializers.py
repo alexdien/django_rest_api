185689f60f5c423e3e404e281e03ae52e8c377cb
Alex Dien <alex.dien@utah.edu>
2022-10-12 16:31:03 -0600

    Init

---
+++ drinks/serializers.py
@@ -0,0 +1,8 @@
+# Describes the process of going from Python object to JSON.
+from rest_framework import serializers
+from .models import Drink
+
+class DrinkSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Drink
+        fields = ['id', 'name', 'description']
